121b69f6f3a22faee3048c400a1e8ace2c071a95
Kagan <kkaytekin@gmail.com>
2022-05-12 07:15:07 +0200

    done loader

--- loader.py
+++ loader.py
@@ -41,51 +41,6 @@ def normalize_pc(pc_np):
     pc_np_norm = pc_np / max_dist
     return pc_np_norm
 
-def voxelize(points, voxel_size=(28, 28, 28), padding_size=(32, 32, 32), resolution=0.1):
-    # Voxelization implementation from VoxNet. Faster.
-    """
-    Convert `points` to centerlized voxel with size `voxel_size` and `resolution`, then padding zero to
-    `padding_to_size`. The outside part is cut, rather than scaling the points.
-    Args:
-    `points`: pointcloud in 3D numpy.ndarray
-    `voxel_size`: the centerlized voxel size, default (24,24,24)
-    `padding_to_size`: the size after zero-padding, default (32,32,32)
-    `resolution`: the resolution of voxel, in meters
-    Ret:
-    `voxel`:32*32*32 voxel occupany grid
-    `inside_box_points`:pointcloud inside voxel grid
-    """
-
-    if abs(resolution) <= 0.0:
-        print('error input, resolution should not be zero')
-        return None, None
-
-    # filter outside voxel_box by using passthrough filter
-    # TODO Origin, better use centroid?
-    origin = (np.min(points[:, 0]), np.min(points[:, 1]), np.min(points[:, 2]))
-    # set the nearest point as (0,0,0)
-    points[:, 0] -= origin[0]
-    points[:, 1] -= origin[1]
-    points[:, 2] -= origin[2]
-    # logical condition index
-    x_logical = np.logical_and((points[:, 0] < voxel_size[0] * resolution), (points[:, 0] >= 0))
-    y_logical = np.logical_and((points[:, 1] < voxel_size[1] * resolution), (points[:, 1] >= 0))
-    z_logical = np.logical_and((points[:, 2] < voxel_size[2] * resolution), (points[:, 2] >= 0))
-    xyz_logical = np.logical_and(x_logical, np.logical_and(y_logical, z_logical))
-    inside_box_points = points[xyz_logical]
-
-    # init voxel grid with zero padding_to_size=(32*32*32) and set the occupancy grid
-    voxels = np.zeros(padding_size)
-    # centerlize to padding box
-    center_points = inside_box_points + (padding_size[0] - voxel_size[0]) * resolution / 2
-    # TODO currently just use the binary hit grid
-    x_idx = (center_points[:, 0] / resolution).astype(int)
-    y_idx = (center_points[:, 1] / resolution).astype(int)
-    z_idx = (center_points[:, 2] / resolution).astype(int)
-    voxels[x_idx, y_idx, z_idx] = 1
-
-    return voxels
-
 def spectify(points):
     ### Complete for task 3
     # Find the spectral embedding of the cloud
@@ -97,8 +52,7 @@ def spectify(points):
                                   n_jobs=-1
                                   )
     points_transformed = embedding.fit_transform(points)
-
-    return emb
+    return np.concatenate((points,points_transformed),axis=-1)
 
 def group_knn(points,seed_points,group_size):
     # Builds a list of point groups around seed_points, can optionally be used
@@ -127,10 +81,7 @@ def mesh_parser(mesh,parse_to):
     if parse_to=='point':
         pc_o3d=o3d.geometry.PointCloud()
         pc_o3d=o3d.geometry.TriangleMesh.sample_points_uniformly(mesh,number_of_points=1024)
-        pc_np=np.asarray(pc_o3d.points)
-        # vis_points(pc_np)
-        pc_np=normalize_pc(pc_np)
-        # vis_points(pc_np)
+        pc_np = normalize_pc(np.asarray(pc_o3d.points))
         return torch.from_numpy(pc_np).to(dtype=torch.float)
     elif parse_to=='voxel':
         ### Complete for task 2
@@ -165,9 +116,8 @@ def mesh_parser(mesh,parse_to):
         pc_o3d=o3d.geometry.PointCloud()
         pc_o3d=o3d.geometry.TriangleMesh.sample_points_uniformly(mesh,number_of_points=1024)
         ### Complete for task 3
-        
-        spect_np=spectify(pc_np)
-        return #
+        spect_np=spectify(np.asarray(pc_o3d.points))
+        return torch.from_numpy(spect_np).to(dtype=torch.float)
 
     
 
@@ -212,35 +162,17 @@ class ChallengeDataset(Dataset):
 
 
 def test_pcd(mesh_o3d):
-    pc_o3d = o3d.geometry.PointCloud()
-    pc_o3d = o3d.geometry.TriangleMesh.sample_points_uniformly(mesh_o3d,number_of_points=1024)
-    pc_o3d.points  = normalize_pc(np.asarray(pc_o3d.points))
-
-    # TODO: advanced test
-    # create 3D bounding box with edge length 1. plot it together with pcd to see if everything fits inside.
-
+    pc_np = mesh_parser(mesh_o3d,'point').numpy()
     vis_points(pc_np)
 
 def test_voxel(mesh_o3d):
-    # Creating from mesh takes forever. create from pcd
-    #vox_o3d=o3d.geometry.VoxelGrid().create_from_triangle_mesh(mesh_o3d, 1)
-    pc_o3d = o3d.geometry.PointCloud()
-    pc_o3d = o3d.geometry.TriangleMesh.sample_points_uniformly(mesh_o3d,number_of_points=4096)
-    # For the dresser, poisson disk sampling yields more complete voxelizations.
-    # But it is slower and the gaps in the voxelized geometries serve as regularization, so I stick to
-    # uniform point sampling.
-    #pc_o3d=o3d.geometry.TriangleMesh.sample_points_poisson_disk(mesh_o3d,number_of_points=4096)
-    pc_o3d.points = o3d.utility.Vector3dVector( normalize_pc( np.asarray(pc_o3d.points)) )
-
-    #pc_o3d= pc_o3d.scale(1 / np.max(pc_o3d.get_max_bound() - pc_o3d.get_min_bound()),
-    #  center=pc_o3d.get_center())
-    voxel_size = 32
-    vox_o3d=o3d.geometry.VoxelGrid().create_from_point_cloud(pc_o3d,2/32)
-
-    voxels = np.zeros((32,32,32))
-    for voxel in vox_o3d.get_voxels():
-        voxels[voxel.grid_index] = 1
+    vox_o3d = mesh_parser(mesh_o3d,'voxel') # TODO: Change the return statement of mesh_parser to use it
     vis_voxel_o3d(vox_o3d)
+
+def test_spectral(mesh_o3d):
+    pc_np = mesh_parser(mesh_o3d,'spectral').numpy()
+    vis_points(pc_np[:,:3])
+    vis_points(pc_np[:,3:])
 
 ex_from_diff_class = {'airplane' : 'airplane_0628.off',
                        'bed' : 'bed_0520.off',
@@ -277,8 +209,7 @@ if __name__ == "__main__":
     for key,value in ex_from_diff_class.items():
         path = os.path.join(os.getcwd(),'dataset',key,'test',value)
         mesh_o3d=o3d.io.read_triangle_mesh(path)
-        test_voxel(mesh_o3d)
-
-    #test_pcd(mesh_o3d)
-
+        #test_voxel(mesh_o3d)
+        #test_pcd(mesh_o3d)
+        test_spectral(mesh_o3d)
 
